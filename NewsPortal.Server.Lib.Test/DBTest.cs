4f985c920e1d7566f185e66bb717668d4848ec59
Starinin Andrey <starininandrey@gmail.com>
2022-04-12 20:34:34 +0300

    Добавление unit-тестов

---
+++ NewsPortal.Server.Lib.Test/DBTest.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NewsPortal.Server.Lib.DataBase;
+using NewsPortal.Server.Lib.Models;
+using Xunit;
+
+namespace NewsPortal.Server.Lib.Test
+{
+    public class DbTest
+    {
+        [Fact]
+        public async Task GetAllNews_Test()
+        {
+            IEnumerable<News> expectedNews = new List<News>
+            {
+                new()
+                {
+                    Id = 1,
+                    Title = "News 1",
+                    Content = "content 1",
+                    DateOfCreation = new DateTime(2022, 4, 12, 18, 48, 51),
+                    Author = 1
+                }
+            };
+
+            var db = new DB();
+            var actualNews = await db.GetAllNews();
+
+            Assert.Equal(expectedNews, actualNews);
+        }
+
+        [Fact]
+        public async Task GetAllUsers_Test()
+        {
+            IEnumerable<User> expectedUsers = new List<User>
+            {
+                new()
+                {
+                    Id = 1,
+                    Login = "admin",
+                    Password = "12345",
+                    Email = "admin@email.ru",
+                    FirstName = "Admin",
+                    LastName = "Admin",
+                    MiddleName = "Admin"
+                }
+            };
+
+            var db = new DB();
+            var actualUsers = await db.GetAllUsers();
+            
+            Assert.Equal(expectedUsers, actualUsers);
+        }
+    }
+}
